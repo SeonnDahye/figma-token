0311111bd4de59bafcfd8c2796325c61c024714e
SeonnDahye <sdh@hconnect.co.kr>
2024-10-30 08:11:54 +0000

    Figma updated styles

--- src/style/theme/variables.js
+++ src/style/theme/variables.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 30 Oct 2024 08:00:47 GMT
+ * Generated on Wed, 30 Oct 2024 08:11:54 GMT
  */
 
 export const ButtonPrimaryBackground = "#5a67d8";
@@ -9,7 +9,7 @@ export const ButtonBorderWidth = 8;
 export const CardBorderRadius = 8;
 export const CardBackground = "#1a202c";
 export const CardPadding = 16;
-export const BoxShadowDefault = [{"x":5,"y":5,"spread":3,"color":"#00000026","blur":5,"$type":"dropShadow"},{"x":4,"y":4,"spread":6,"color":"#00000033","blur":5,"$type":"innerShadow"}];
+export const BoxShadowDefault = [{"x":5,"y":5,"spread":4,"color":"#00000026","blur":5,"$type":"dropShadow"},{"x":4,"y":4,"spread":6,"color":"#00000033","blur":5,"$type":"innerShadow"}];
 export const TypographyH1Bold = {"fontFamily":"Inter","fontWeight":"Bold","lineHeight":"120%","fontSize":48.829,"paragraphSpacing":32,"letterSpacing":"-5%"};
 export const TypographyH1Regular = {"fontFamily":"Inter","fontWeight":"Regular","lineHeight":"120%","fontSize":48.829,"paragraphSpacing":32,"letterSpacing":"-5%"};
 export const TypographyH2Bold = {"fontFamily":"Inter","fontWeight":"Bold","lineHeight":"120%","fontSize":39.063,"paragraphSpacing":26,"letterSpacing":"-5%"};
